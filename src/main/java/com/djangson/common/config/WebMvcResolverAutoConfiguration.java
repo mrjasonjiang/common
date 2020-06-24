e7f07743360478a0ec37d866dce007c4fc6e11a5
jiangjingsheng <jiangjingsheng@vichain.com>
2020-06-24 14:11:40 +0800

    config

--- src/main/java/com/djangson/common/config/WebMvcResolverAutoConfiguration.java
+++ src/main/java/com/djangson/common/config/WebMvcResolverAutoConfiguration.java
@@ -1,9 +1,9 @@
 package com.djangson.common.config;
 
+import com.djangson.common.context.converter.WebConverterConfig;
+import com.djangson.common.context.converter.WebJacksonConfig;
+import com.djangson.common.context.handler.WebMvcExceptionHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.vichain.common.context.converter.WebConverterConfig;
-import com.vichain.common.context.converter.WebJacksonConfig;
-import com.vichain.common.context.handler.WebMvcExceptionHandler;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
